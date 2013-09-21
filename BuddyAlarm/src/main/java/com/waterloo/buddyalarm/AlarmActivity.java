f3d618854551ba637873123268a84966580a6f2b
Ando <andersen.yang@xtremelabs.com>
2013-09-21 16:38:43 -0400

    Changed layout of alarmActivity

--- BuddyAlarm/src/main/java/com/waterloo/buddyalarm/AlarmActivity.java
+++ BuddyAlarm/src/main/java/com/waterloo/buddyalarm/AlarmActivity.java
@@ -2,14 +2,22 @@ package com.waterloo.buddyalarm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Andersen on 21/09/13.
  */
 public class AlarmActivity extends Activity {
+    private Window w;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.alarm_page);
+        w = this.getWindow();
+        w.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        w.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        w.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 }
